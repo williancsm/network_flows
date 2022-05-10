00c8889ed8ef053064ed67db6382c75fe5a9846d
williancsm <>
2022-05-10 17:43:10 +0000

    -added example

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,36 @@
 #include <iostream>
 
 #include "graph.hpp"
+#include "dijkstra.hpp"
 
 using namespace std;
 
-int main(void) {
-	Graph_Library::Graph My_Graph;
+int main(void) {	
+	//https://www.geeksforgeeks.org/wp-content/uploads/Fig-11.jpg
+	int V = 9;
+	Graph::Graph My_Graph(V);
 
-	cout << My_Graph.Get_Number_Of_Vertices() << endl;
-	cout << My_Graph.Get_Number_Of_Edges() << endl;
+	My_Graph.Add_Edge(0, 1, 4.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(0, 7, 8.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(1, 7, 11.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(1, 2, 8.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(2, 8, 2.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(7, 8, 7.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(7, 6, 1.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(8, 6, 6.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(6, 5, 2.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(2, 5, 4.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(2, 3, 7.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(3, 5, 14.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(3, 4, 9.0, Graph::Graph_Type::Edge_Type::Undirected);
+	My_Graph.Add_Edge(4, 5, 10.0, Graph::Graph_Type::Edge_Type::Undirected);
+
+
+	cout << "Vertices: " << My_Graph.Get_Number_Of_Vertices() << endl;
+	cout << "Edges: " << My_Graph.Get_Number_Of_Edges() << endl;
+  cout << "Adjacent List: " << endl;
+	My_Graph.Print_Adjacent_List();
+	
 
 	return 0;
 }
