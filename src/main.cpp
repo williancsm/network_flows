ee272654f11077658051c3ff31d69f944439036e
williancsm <>
2022-05-17 22:09:18 +0000

    updated example

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(void) {
 	My_Graph.Print_Adjacent_List();
 
 	Algorithms::Dijkstra::Dijkstra My_Dijkstra(My_Graph, 4, 0);
-	My_Dijkstra.Start_2();
+	My_Dijkstra.Start();
 	My_Dijkstra.Print_Path();
 
 
